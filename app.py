f4184e2f4ae256771cfc8526b6f9529784083315
Panisara06625 <157197807+Panisara06625@users.noreply.github.com>
2025-08-17 14:07:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if len(row_data) >= 10:
         predicted_percent = int(prob_safe * 100)
 
         # ลบแถวที่ 1 หลังประมวลผล
-        sheet.delete_rows(2)
+        sheet.delete_rows(1)
 
     except Exception as e:
         st.error(f"Prediction error: {e}")
